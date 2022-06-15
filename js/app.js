d3bef233ddd92b995c62a63ba41ae0657d3742b9
Eslam Magdy <eslam3400@gmail.com>
2022-06-15 19:41:09 +0200

    update github

--- js/app.js
+++ js/app.js
@@ -1,32 +1,5 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
- */
-
-/**
- * Define Global Variables
- * 
- */
-const navbar = document.querySelector('#navbar__list');
-const sections = document.querySelectorAll('section')
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
- */
-
-// My JS
+const eNav = document.querySelector('#navbar__list');
+const eSec = document.querySelectorAll('section')
 
 const helloWorld = document.getElementById('hello-world');
 setInterval(() => {
@@ -40,82 +13,57 @@ setInterval(() => {
     }, 200);
 }, 6000);
 
-// check which element is active
-function getActiveElem() {
-    maxSection = sections[0];
-    minVal = 1000000;
-    for (item of sections) {
-        let bounding = item.getBoundingClientRect();
-        if (bounding.top > -300 & bounding.top < minVal) {
-            minVal = bounding.top;
-            maxSection = item;
+function makeNav() {
+    for (let link of eSec) {
+        let link = document.createElement('li');
+        link.className = 'menu__link';
+        link.dataset.nav = link.id;
+        link.innerText = link.dataset.nav;
+        eNav.appendChild(link);
+    };
+};
+
+function goToSec() {
+    eNav.addEventListener('click', function (event) {
+        const target = document.querySelector('#' + event.target.dataset.nav)
+        target.scrollIntoView();
+    });
+};
+
+function activeElement() {
+    endSec = eSec[0];
+    mVal = 1000000;
+    for (section of eSec) {
+        let border = section.getBoundingClientRect();
+        if (border.top > -300 & border.top < mVal) {
+            mVal = border.top;
+            endSec = section;
         };
     };
-    return maxSection;
+    return endSec;
 };
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
- */
-
-// build the nav
-function addSections() {
-    for (let item of sections) {
-        let section = document.createElement('li');
-        section.className = 'menu__link';
-        section.dataset.nav = item.id;
-        section.innerText = item.dataset.nav;
-        navbar.appendChild(section);
-    };
-};
-
-// Add class 'active' to section when near top of viewport
-function setActive() {
+function makeActive() {
     window.addEventListener('scroll', function (event) {
-        let section = getActiveElem();
-        section.classList.add('your-active-class');
-        // set other sections as inactive
-        for (let item of sections) {
-            if (item.id != section.id & item.classList.contains('your-active-class')) {
-                item.classList.remove('your-active-class');
+        let section = activeElement();
+        section.classList.add('active-class');
+        for (let sSection of eSec) {
+            if (sSection.id != sSection.id & sSection.classList.contains('active-class')) {
+                sSection.classList.remove('active-class');
             }
         }
-        // set corresponding header style
-        const active = document.querySelector('li[data-nav="' + section.id + '"]');
-        active.classList.add('active__link');
-        // remove from other headers
-        const headers = document.querySelectorAll('.menu__link');
-        for (let item of headers) {
-            console.log(item);
-            if (item.dataset.nav != active.dataset.nav & item.classList.contains('active__link')) {
-                item.classList.remove('active__link');
+        const activeSec = document.querySelector('li[data-nav="' + section.id + '"]');
+        activeSec.classList.add('active__link');
+        const menuLink = document.querySelectorAll('.menu__link');
+        for (let link of menuLink) {
+            console.log(link);
+            if (link.dataset.nav != activeSec.dataset.nav & link.classList.contains('active__link')) {
+                link.classList.remove('active__link');
             }
         };
     });
 };
 
-// Scroll to anchor ID using scrollTO event
-function scrollToClick() {
-    navbar.addEventListener('click', function (event) {
-        const clicked = document.querySelector('#' + event.target.dataset.nav)
-        clicked.scrollIntoView();
-    });
-};
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
- */
-
-// Build menu 
-addSections();
-
-// Scroll to section on link click
-scrollToClick();
-
-// Set sections as active
-setActive();
+makeNav();
+goToSec();
+makeActive();
